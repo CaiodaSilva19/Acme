ac350fd26860eba53927873464a7eed584a63945
CaiodaSilva19 <caiodasilvacorreialima@gmail.com>
2026-04-25 04:40:56 +0000

    fix: incluir dados da requisicao nos parametros das funcoes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+  prisma: PrismaClient | undefined
 }
 
-export default prisma;
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
+
+export default prisma
